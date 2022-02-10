a7d3923734fc9f647e47a5cb3b355c27836227be
Your Name <you@example.com>
2022-02-10 12:18:10 +0100

    footer done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-      
+  <div class="footer_container">
+      <div class="social">
+        <img src="../assets/layout-images/images/logo-restaurant.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sed eaque mollitia inventore a nisi qui asperiores.</p>
+        <ul>
+          <li v-for="(element, index) in socialArr" :key="index"><i :class="element" class="social_elem"></i></li>
+        </ul>
+      </div>
+      <div class="copy_container">
+        <div class="copy">
+          Copyright 2021 - 2019 Avada | All rights reserved | Powered by WordPress | ThemeFusion
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
     name: 'Footer',
+    data(){
+      return{
+        socialArr: [
+          "fa-brands fa-facebook-f", 
+          "fa-brands fa-twitter", 
+          "fa-brands fa-youtube", 
+          "fa-brands fa-instagram",
+          "fa-brands fa-linkedin-in"
+        ]
+      }
+    }
 }
+
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "../assets/footer.scss";
 </style>
